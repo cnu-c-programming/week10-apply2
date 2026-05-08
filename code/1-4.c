b9d414d4743fe1d8472d08a3e08e71f5c6a570ba
apply2 <67138665+apply2@users.noreply.github.com>
2026-05-08 11:35:17 +0900

    Update 1-4.c

--- code/1-4.c
+++ code/1-4.c
@@ -27,6 +27,9 @@ int main() {
         update_val(s);
     }
     finish = clock();
+    printf("%f\n", (double)(finish-start)/CLOCKS_PER_SEC);
+
+    start = clock();
     for (int i = 0; i < 1e7; i++)
     {
         update_ptr(&s);
