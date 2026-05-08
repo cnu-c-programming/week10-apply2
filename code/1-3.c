a2785d729946e1e986fe61af2880210a6188b378
apply2 <67138665+apply2@users.noreply.github.com>
2026-05-08 11:31:57 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -14,7 +14,7 @@ void update_val(struct IPv4 s)
 };
 
 void update_ptr(struct IPv4* s) {
-    sprintf(s->data, "update_val");
+    sprintf(s->data, "update_ptr");
 }
 int main() {
     struct IPv4 s1, s2 = {0};
